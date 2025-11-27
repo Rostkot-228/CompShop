c8b837baacde6bfd8b68685c313ea8982c3d9e7f
Ростислав Мошко <koshakcrosts@gmail.com>
2025-11-27 22:48:39 +0300

    Create DB

---
+++ Net21/WebPortal/Migrations/20251127194540_CreateDB.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleasDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categoryes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoryes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Festival",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Slogan = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Theme = table.Column<int>(type: "int", nullable: false),
+                    LogoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Festival", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeDevices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeDevices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AvatarUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Money = table.Column<int>(type: "int", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    Language = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TypeDeviceId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    IsPopular = table.Column<bool>(type: "bit", nullable: false),
+                    CategoryEnum = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_Categoryes_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categoryes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Devices_TypeDevices_TypeDeviceId",
+                        column: x => x.TypeDeviceId,
+                        principalTable: "TypeDevices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Girls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: true),
+                    AuthorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Girls", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Girls_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    LevelNotification = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Computers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Processor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ram = table.Column<int>(type: "int", nullable: false),
+                    Memory = table.Column<double>(type: "float", nullable: false),
+                    VideoCard = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Motherboard = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PowerUnit = table.Column<int>(type: "int", nullable: false),
+                    DeviceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Computers_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimeGirl",
+                columns: table => new
+                {
+                    AnimesId = table.Column<int>(type: "int", nullable: false),
+                    CharactersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimeGirl", x => new { x.AnimesId, x.CharactersId });
+                    table.ForeignKey(
+                        name: "FK_AnimeGirl_Animes_AnimesId",
+                        column: x => x.AnimesId,
+                        principalTable: "Animes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AnimeGirl_Girls_CharactersId",
+                        column: x => x.CharactersId,
+                        principalTable: "Girls",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FestivalGirl",
+                columns: table => new
+                {
+                    FestivalsId = table.Column<int>(type: "int", nullable: false),
+                    GirlsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FestivalGirl", x => new { x.FestivalsId, x.GirlsId });
+                    table.ForeignKey(
+                        name: "FK_FestivalGirl_Festival_FestivalsId",
+                        column: x => x.FestivalsId,
+                        principalTable: "Festival",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FestivalGirl_Girls_GirlsId",
+                        column: x => x.GirlsId,
+                        principalTable: "Girls",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GirlUser",
+                columns: table => new
+                {
+                    FavoriteGirlsId = table.Column<int>(type: "int", nullable: false),
+                    UserWhoAddToFavoriteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GirlUser", x => new { x.FavoriteGirlsId, x.UserWhoAddToFavoriteId });
+                    table.ForeignKey(
+                        name: "FK_GirlUser_Girls_FavoriteGirlsId",
+                        column: x => x.FavoriteGirlsId,
+                        principalTable: "Girls",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GirlUser_Users_UserWhoAddToFavoriteId",
+                        column: x => x.UserWhoAddToFavoriteId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotificationUser",
+                columns: table => new
+                {
+                    UserWhoViewedItId = table.Column<int>(type: "int", nullable: false),
+                    ViewedNotificationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationUser", x => new { x.UserWhoViewedItId, x.ViewedNotificationId });
+                    table.ForeignKey(
+                        name: "FK_NotificationUser_Notifications_ViewedNotificationId",
+                        column: x => x.ViewedNotificationId,
+                        principalTable: "Notifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NotificationUser_Users_UserWhoViewedItId",
+                        column: x => x.UserWhoViewedItId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimeGirl_CharactersId",
+                table: "AnimeGirl",
+                column: "CharactersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Computers_DeviceId",
+                table: "Computers",
+                column: "DeviceId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_CategoryId",
+                table: "Devices",
+                column: "CategoryId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_TypeDeviceId",
+                table: "Devices",
+                column: "TypeDeviceId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FestivalGirl_GirlsId",
+                table: "FestivalGirl",
+                column: "GirlsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Girls_AuthorId",
+                table: "Girls",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GirlUser_UserWhoAddToFavoriteId",
+                table: "GirlUser",
+                column: "UserWhoAddToFavoriteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_AuthorId",
+                table: "Notifications",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationUser_ViewedNotificationId",
+                table: "NotificationUser",
+                column: "ViewedNotificationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimeGirl");
+
+            migrationBuilder.DropTable(
+                name: "Computers");
+
+            migrationBuilder.DropTable(
+                name: "FestivalGirl");
+
+            migrationBuilder.DropTable(
+                name: "GirlUser");
+
+            migrationBuilder.DropTable(
+                name: "News");
+
+            migrationBuilder.DropTable(
+                name: "NotificationUser");
+
+            migrationBuilder.DropTable(
+                name: "Animes");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Festival");
+
+            migrationBuilder.DropTable(
+                name: "Girls");
+
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "Categoryes");
+
+            migrationBuilder.DropTable(
+                name: "TypeDevices");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
